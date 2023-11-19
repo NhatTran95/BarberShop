46795c8d243eea7fb63cb84aab6631d5588d13e3
NhatTran95 <tranducanhnhat@gmail.com>
2023-11-20 06:26:21 +0700

    up

---
+++ src/main/resources/static/assets/js/login.js
@@ -0,0 +1,15 @@
+
+$("#signup-box-link").click(function(){
+    $("#login").fadeOut(100);
+    $("#register").delay(100).fadeIn(100);
+    $("#login-box-link").removeClass("active white-btn");
+    $("#signup-box-link").addClass("active white-btn");
+});
+$("#login-box-link").click(function(){
+    $("#login").delay(100).fadeIn(100);;
+    $("#register").fadeOut(100);
+    $("#login-box-link").addClass("active white-btn");
+    $("#signup-box-link").removeClass("active white-btn");
+
+});
+
